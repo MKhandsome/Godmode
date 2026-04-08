69962d7790447f0acff5c862a7b76e5a51c463c6
MKhandsome <106364691+MKhandsome@users.noreply.github.com>
2026-04-08 21:05:34 +0700

    Add Next.js home page component

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Next.js Home Page!</h1>
+            <p>This is a simple homepage built using Next.js.</p>
+        </div>
+    );
+};
+
+export default HomePage;
